be6067143f7c26c3bcadb92e5dc38f8bb5a63467
moraj20 <moraj20@farmingdale.edu>
2025-07-08 22:21:16 -0400

    Update Student.java

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
@@ -1,19 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.csc325_oop_designreview_lab;
 
-/**
- *
- * @author MoaathAlrajab
- */
-public class Student {
-	// ToDo 1: Make this class a child of Human
+public class Student extends Human {
+    private double gpa;
+    private String address;
+    private int credits;
 
-	// ToDo 2: Fix the resulting errors
+    public Student(String name, int age, int credits) {
+        super(name, (short) age);
+        this.credits = credits;
+    }
 
-	// ToDo 3: Add a field for GPA and create a setter and a getter
-	
-	// ToDo 4: Add comments to your code
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    @Override
+    public String getAddress() {
+        return address;
+    }
+
+    @Override
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public void setCredits(int credits) {
+        this.credits = credits;
+    }
+
+    @Override
+    public String toString() {
+        return "Student: " + getName() + ", Age: " + getAge() + ", GPA: " + gpa + ", Credits: " + credits;
+    }
 }
