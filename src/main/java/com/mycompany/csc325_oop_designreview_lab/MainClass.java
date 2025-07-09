a894cbd22717f2cef9280bcb68baeeabeb131050
moraj20 <moraj20@farmingdale.edu>
2025-07-08 22:17:14 -0400

    Update MainClass.java

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/MainClass.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/MainClass.java
@@ -1,44 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.csc325_oop_designreview_lab;
 
-/**
- *
- * @author MoaathAlrajab
- */
-public class MainClass {
+public class Student extends Human {
+    private int credits;
+    private double gpa;
+    private String address;
 
- public static void main(String[] args) {
-		// ToDo 5: Fix the error
+    public Student(String name, int age, int credits) {
+        super(name, (short) age);
+        this.credits = credits;
+    }
 
-		// ToDo 6: Fix the constructor of the Student class
+    public int getCredits() {
+        return credits;
+    }
 
-                // Todo 7: Create two classes for Freshman and Senior 
+    public void setCredits(int credits) {
+        this.credits = credits;
+    }
 
-                // ToDo 8: The senior class should have a minimum of 85 credits  
+    public double getGpa() {
+        return gpa;
+    }
 
-		// ToDo 9: Add a toString method for the Student class
-		// ToDo 10: Add a toString method for the Freshman class
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
 
-		Student std1= new Student("James", 20);
-                // ToDo 11: Add a toString method for the Senior class
+    @Override
+    public String getAddress() {
+        return address;
+    }
 
-		Freshman std1= new Student("James", 20, 12); // name, age, credits
+    @Override
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
-                Senior std2 = new Student("John", 30, 90);
-
-		// ToDo 12: Set the gpa of the student using the scanner and user
-		// 			input and then print the output.
-
-		System.out.println(std1);
-
-                System.out.println(std2);
-
-		// ToDo 13: add comments and explain your code
-
-	}
-
+    @Override
+    public String toString() {
+        return "Student: " + getName() + ", Age: " + getAge() + ", Credits: " + credits + ", GPA: " + gpa;
+    }
 }
 
