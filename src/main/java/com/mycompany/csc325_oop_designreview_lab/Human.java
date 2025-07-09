b1c215795a20caa52327dbac4545fcaf42bb7b6b
moraj20 <moraj20@farmingdale.edu>
2025-07-08 22:13:47 -0400

    Update Human.java

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Human.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Human.java
@@ -1,42 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.csc325_oop_designreview_lab;
 
-/**
- *
- * @author MoaathAlrajab
- */
-public abstract class Human {
-    private String name;
-	private String address;
-	private short age;
+public class Student extends Human {
+    private double gpa;
+    private String address;
 
-	// constructor that takes only two paras
-	public Human(String name, short age) {
-		this.name = name;
-		this.age = age;
-	}
+    public Student(String name, short age, double gpa, String address) {
+        super(name, age);
+        this.gpa = gpa;
+        this.address = address;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public double getGpa() {
+        return gpa;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
 
-	public abstract String getAddress() ;
+    @Override
+    public String getAddress() {
+        return address;
+    }
 
-	public abstract void setAddress(String address);
+    @Override
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
-
-	public short getAge() {
-		return age;
-	}
-
-	public void setAge(short age) {
-		this.age = age;
-	}
+    @Override
+    public String toString() {
+        return "Name: " + getName() + ", Age: " + getAge() + ", GPA: " + gpa + ", Address: " + address;
+    }
 }
